80541d1843a7857df4845df989a6045bc15b72e5
GRANDSOFT\lizs-b <lizs-b@glodon.com>
2022-09-23 13:45:52 +0800

    master-a0

--- aaa.js
+++ aaa.js
@@ -1,16 +1,3 @@
 alert("hello-e2")
 
-console.log('master-a2')
-
-console.log('zli-a3')
-
-console.log('zli-b3')
-
-
-console.log('zli-c1')
-console.log('zli-c2')
-console.log('zli-c3')
-
-console.log('zli-d1')
-console.log('zli-d2')
-console.log('zli-d3')
+console.log('master-a0')
